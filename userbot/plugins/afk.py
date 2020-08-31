5f287dbcbd40909fcc5d474d163b01a4f0520e36
anantaroy7 <70362366+anantaroy7@users.noreply.github.com>
2020-08-31 12:43:55 +0530

    Madam

--- userbot/plugins/afk.py
+++ userbot/plugins/afk.py
@@ -104,7 +104,7 @@ async def on_afk(event):
             else:
                 afk_since = f"`{int(seconds)}s` **ago**"
         msg = None
-        message_to_reply = f"__My Master Has Been In afk For__ `{total_afk_time}`\nWhere She Is: ONLY He KNOWS " + \
+        message_to_reply = f"__My Madam Has Been In afk For__ `{total_afk_time}`\nWhere She Is: ONLY He KNOWS " + \
             f"\n\n__I promise She'll back in a few light years__\n**REASON**: {reason}" \
             if reason \
             else f"**Heya!**\n__I am currently unavailable. Since when, you ask? For {total_afk_time} I guess.__\n\nWhen will I be back? Soon __Whenever I feel like it__**( ಠ ʖ̯ ಠ)**  "
